af718e477e744edfd79a64ad6bfe7e044400b493
Kelly Popko <kelpopko@gmail.com>
2024-10-02 10:02:24 -0500

    Iteration 7
    Simplify attr_*:
    - Remove public readers
    - Turn private writers into accessors
    
    Simplify update_pattern:
    - remove check for complete_match *because* that is checked prior to
      update_pattern being called.
    - This allows ternary use
    
    Change to approach setting 'flags':
    - Now iterates through flags passed, sets attributes to true.
    
    - Other flag-related attributes remain nil.  (Previously all
      flag-related attributes were set to false if not true)
    
    - Change name of grep instance from new_grepper to instance.

--- grep.rb
+++ grep.rb
@@ -19,43 +19,45 @@ class Grep
   private_constant :FLAGS
 
   def self.grep(pattern, flags, files)
-    new_grepper = new(flags, pattern, files.count > 1)
+    instance = new(flags, pattern, files.count > 1)
 
-    files.map { |file| new_grepper.grep(file) }.reject(&:empty?).join("\n")
+    files.map { |file| instance.grep(file) }.reject(&:empty?).join("\n")
   end
 
   private
 
   attr_reader :file_name_needed
 
-  attr_writer :pattern,
+  attr_accessor :pattern,
               :case_insensitive,
               :line_number_needed,
               :file_name_only,
               :invert_match,
               :complete_line_match
 
+  alias case_insensitive? case_insensitive
+  alias line_number_needed? line_number_needed
+  alias file_name_only? file_name_only
+  alias invert_match? invert_match
+  alias complete_line_match? complete_line_match
+  alias file_name_needed? file_name_needed
+
   def initialize(flags, raw_pattern, multiple_files)
     @file_name_needed = multiple_files
     self.pattern = raw_pattern
 
-    # Toggle flags, update pattern if needed
+    # Set attributes corresponding to flags passed to true
     update_flag_attributes(flags)
-    update_pattern if case_insensitive || complete_line_match
+    # Update pattern if needed (e.g. flag -i, -v)
+    update_pattern if case_insensitive? || complete_line_match?
   end
 
   def update_flag_attributes(flags)
-    FLAGS.each do |flag, attribute|
-      send("#{attribute}=", flags.include?(flag))
-    end
+    flags.each { |flag| send("#{FLAGS[flag]}=", true) }
   end
 
   def update_pattern
-    self.pattern = if case_insensitive
-                     /#{pattern}/i
-                   elsif complete_line_match
-                     /^#{pattern}$/
-                   end
+    self.pattern = case_insensitive? ? /#{pattern}/i : /^#{pattern}$/
   end
 
   def file_lines(file)
@@ -63,31 +65,22 @@ class Grep
   end
 
   def matches_pattern?(line)
-    invert_match ? !line.match?(pattern) : line.match?(pattern)
+    invert_match? ? !line.match?(pattern) : line.match?(pattern)
   end
 
   def format_line(file, number, line)
-    file_name = file_name_needed ? "#{file}:" : ''
-    line_number = line_number_needed ? "#{number}:" : ''
+    file_name = file_name_needed? ? "#{file}:" : ''
+    line_number = line_number_needed? ? "#{number}:" : ''
     file_name + line_number + line
   end
 
   public
 
-  attr_reader :pattern,
-              :case_insensitive,
-              :line_number_needed,
-              :file_name_only,
-              :invert_match,
-              :complete_line_match
-
-  attr_accessor :file
-
   def grep(file_name)
     file_lines(file_name).each_with_object([]).with_index do |(line, lines), i|
       next unless matches_pattern?(line)
 
-      lines << if file_name_only
+      lines << if file_name_only?
                  next if lines.include?(file_name)
 
                  file_name
